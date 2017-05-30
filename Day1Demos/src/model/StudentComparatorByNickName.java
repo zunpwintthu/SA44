9d4a9f983dc28d9fe6d6b01bd78bed91802dc735
suriarasai <suria.r.asai@gmail.com>
2017-05-30 13:44:50 +0800

    Test Class

--- Day1Demos/src/model/StudentComparatorByNickName.java
+++ Day1Demos/src/model/StudentComparatorByNickName.java
@@ -7,7 +7,7 @@ public class StudentComparatorByNickName implements Comparator<Student> {
 	@Override
 	public int compare(Student o1, Student o2) {
 		// TODO Auto-generated method stub
-		return o1.getName().compareTo(o2.getName());
+		return o1.getNickName().compareTo(o2.getNickName());
 	}
 
 }
